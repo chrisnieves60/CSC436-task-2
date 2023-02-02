fb7e059c8aa9137f1a21224ff7d64bcce3a54df8
chrisnieves60 <chrisnieves60@gmail.com>
2023-02-01 23:57:34 -0500

    hw

--- utils/event.js
+++ utils/event.js
@@ -1,8 +1,10 @@
 import { add } from "./math";
+import { multiply } from "./math";
 
 const eventHandler = () => {
 
     const addButton = document.getElementById('add');
+    const multiplyButton = document.getElementById('multiply');
     const number1 = document.getElementById('number-1');
     const number2 = document.getElementById('number-2');
     const result = document.getElementById('result');
@@ -11,8 +13,14 @@ const eventHandler = () => {
             // parseInt(number1.value, 10)+parseInt(number2.value,10);
             add(number1.value,number2.value);
     }
+    const updateDomWithMultipliedNumbers = () => {
+        result.innerHTML = 
+            // parseInt(number1.value, 10)*parseInt(number2.value,10);
+            multiply(number1.value,number2.value);
+    }
 
     addButton.addEventListener('click', updateDomWithAddedNumbers);
+    multiplyButton.addEventListener('click', updateDomWithMultipliedNumbers);
 
 }
 
